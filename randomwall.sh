9e370cea794b57e4231916cd4dd15af64d62ab7d
GoDzM4TT3O <saloniamatteo@pm.me>
2020-05-03 19:50:01 +0200

    Update randomwall.sh

--- randomwall.sh
+++ randomwall.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-# random wallpaper
-paper=$(ls ~/wallpaps | sort -R | tail -1)
-# set random chosen wallpaper as current wallpaper
-hsetroot -cover ~/wallpapers/$paper
+while true
+do 
+  # choose a random wallpaper
+  pape=$(ls ~/wallpapers | sort -R | tail -1)
+  # set the random wallpaper
+	hsetroot -cover ~/wallpapers/$pape
+  # wait 3 minutes (180 seconds)
+	sleep 180
+done
