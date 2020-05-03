dce20f7f25f7400a35f7ebe64122820063f07601
GoDzM4TT3O <saloniamatteo@pm.me>
2020-05-03 19:50:57 +0200

    fix tabs

--- randomwall.sh
+++ randomwall.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 while true
-do 
-  # choose a random wallpaper
-  pape=$(ls ~/wallpapers | sort -R | tail -1)
-  # set the random wallpaper
+do
+	# choose a random wallpaper
+	pape=$(ls ~/wallpapers | sort -R | tail -1)
+	# set the random wallpaper
 	hsetroot -cover ~/wallpapers/$pape
-  # wait 3 minutes (180 seconds)
+	# wait 3 minutes (180 seconds)
 	sleep 180
 done
