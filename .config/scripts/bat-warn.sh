6bb95ac7f4f092921cd4df3b1ad7035d95fcdf0f
GoDzM4TT3O <saloniamatteo@pm.me>
2020-06-03 13:51:22 +0200

    fixed extra spaces

--- .config/scripts/bat-warn.sh
+++ .config/scripts/bat-warn.sh
@@ -12,7 +12,7 @@ do
 	batteryname=$(echo $battery | grep -Eo "BAT[0-9]")
 
 	# if capacity is less than 10% and the battery isn't charging, send a notification (will go away in 30 seconds)
-	[[ $capacity < 10 && $status = "Discharging" || $status = "Unknown" ]] && /usr/bin/notify-send -u critical - t 30000 "Battery $batteryname charge very low  " "Please charge battery $batteryname, its capacity is $capacity%." || \
+	[[ $capacity < 10 && $status = "Discharging" || $status = "Unknown" ]] && /usr/bin/notify-send -u critical -t 30000 "Battery $batteryname charge very low " "Please charge battery $batteryname, its capacity is $capacity%."
 	
 	# if capacity is less than 20% and the battery isn't charging, send a notification (will go away in 30 seconds)
 	[[ $capacity < 20 && $status = "Discharging" || $status = "Unknown" ]] && /usr/bin/notify-send -u critical -t 30000 "Battery $batteryname charge low " "Please charge battery $batteryname, its capacity is $capacity%."
