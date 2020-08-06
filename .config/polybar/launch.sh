7044010674b5ce3e13716391bb6154122d13b133
GoDzM4TT3O <saloniamatteo@protonmail.com>
2020-08-06 12:03:15 +0200

    Changed polybar startup script: If pulseaudio is already started, kill it, then start it again.

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,8 +4,12 @@
 # terminate already running bars
 killall -q polybar
 
+# terminate pulseaudio server and start it again
+killall -q pulseaudio
+
 # launch nord bar
 echo "---" | tee -a /tmp/polybar-nord.log
-polybar nord >> /tmp/polybar-nord.log 2>&1 &
+pulseaudio --start && echo "Started pulseaudio" | tee -a /tmp/polybar-nord.log
+polybar nord >> /tmp/polybar-nord.log 2>&1 & 
 
 echo "Launched nord bar..."
