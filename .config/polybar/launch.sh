b72bd4047c47e92b89b90213b33ada40278cae9a
GoDzM4TT3O <saloniamatteo@pm.me>
2020-11-05 12:22:16 +0100

    Create new bar and fix script

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 # polybar launch script
 
+setxkbmap it
+loadkeys it
+
 # terminate already running bars
 killall -q polybar
 
 # terminate pulseaudio server and start it again
 killall -q pulseaudio
 
-# launch nord bar
-echo "---" | tee -a /tmp/polybar-nord.log
-pulseaudio --start && echo "Started pulseaudio" | tee -a /tmp/polybar-nord.log
-polybar nord >> /tmp/polybar-nord.log 2>&1 & 
+# launch dogrun bar
+echo "---" | tee -a /tmp/polybar-dogrun.log
+pulseaudio --start && echo "Started pulseaudio" | tee -a /tmp/polybar-dogrun.log
+polybar dogrun >> /tmp/polybar-dogrun.log 2>&1 & 
 
-echo "Launched nord bar..."
+echo "Launched dogrun bar..."
